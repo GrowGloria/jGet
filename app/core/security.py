f7b55ae3da887906f565038df12b25c1fc79f871
Rostislav <rost3863@gmail.com>
2026-02-13 22:16:33 +0300

    fix password

--- app/core/security.py
+++ app/core/security.py
@@ -1,7 +1,7 @@
 ﻿import hashlib
 from passlib.context import CryptContext
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["bcrypt_sha256", "bcrypt"], deprecated="auto")
 
 
 def hash_password(password: str) -> str:
